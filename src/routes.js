0e85eddfaa695c01ceac72a5d6939ed93b87843f
Gabriel Henrique <gabrielhenrique@MacBook-Air-de-Gabriel.local>
2019-07-27 16:05:43 -0300

    erro length

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,22 @@
 import { Router } from 'express';
+
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
+import FileController from './app/controller/FileController';
+
 import authMiddleware from './app/middlewares/auth';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.post('/files', upload.single('file'), FileController.store);
 routes.put('/users', UserController.update);
 
 export default routes;
